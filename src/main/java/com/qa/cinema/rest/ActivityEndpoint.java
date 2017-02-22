c3d71b91d8b9e58ed0fef4c33c0c9be1f9060e17
Jack Gallacher <jackleslie.gallacher@gmail.com>
2017-02-22 20:55:19 +0000

    QC-83 Task Completed

--- src/main/java/com/qa/cinema/rest/ActivityEndpoint.java
+++ src/main/java/com/qa/cinema/rest/ActivityEndpoint.java
@@ -1,9 +1,15 @@
 package com.qa.cinema.rest;
 
+import javax.inject.Inject;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+
+import com.qa.cinema.service.ActivityService;
 
 @Path("/activity")
 public class ActivityEndpoint {
@@ -25,14 +31,14 @@ public class ActivityEndpoint {
 	}
 	
 	@PUT
-	@Path("/json/{id}")
+	@Path("/json/{activityID}")
 	@Produces({ "application/json" })
 	public String updateActivityAsJSON(@PathParam("activityID") Long activityID, String activityJSON){
 		return activityService.updateActivity(activityID, activityJSON);
 	}
 	
 	@DELETE
-	@Path("/json/{id}")
+	@Path("/json/{activityID}")
 	@Produces({ "application/json" })
 	public String deleteActivityAsJSON(@PathParam("activityID") Long activityID){
 		return activityService.deleteActivity(activityID);	
--- src/test/java/com/qa/cinema/rest/test/ActivityEndpointTest.java
+++ src/test/java/com/qa/cinema/rest/test/ActivityEndpointTest.java
@@ -1,7 +1,6 @@
 package com.qa.cinema.rest.test;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -9,39 +8,51 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import com.qa.cinema.rest.ActivityEndpoint;
+import com.qa.cinema.service.ActivityService;
 
-
+@RunWith(MockitoJUnitRunner.class)
 public class ActivityEndpointTest {
 	
 	@InjectMocks
-	private ActivityEndPoint subject;
+	private ActivityEndpoint subject;
 	
 	private static final String MOCKSTRING = "[{\"activityID\": 1,\"title\": \"testTitle\",\"description\": \"testDescription\",\"releaseDate\": \"19901212\",\"certification\": \"18\"}]";
 	private static final String MOCK_DELETE_MESSAGE = "{\"message\": \"Activity sucessfully removed\"}";
 	
 	@Mock
-	private ActivityService activityService;
-	
-	
-		
+	private ActivityService mockService;
+			
 	@Test
 	public void testlistAllActivities() {
-		fail("Not yet implemented");
+		Mockito.when(mockService.listAllActivities()).thenReturn(MOCKSTRING);
+		String activityString = subject.listAllActivitiesAsJSON();
+		Mockito.verify(mockService).listAllActivities();
+		Assert.assertEquals(MOCKSTRING, activityString);	
 	}
 	
 	@Test
 	public void testcreateActivity() {
-		fail("Not yet implemented");
+		Mockito.when(mockService.createActivity(MOCKSTRING)).thenReturn(MOCKSTRING);	
+		String activityString = subject.createActivityAsJSON(MOCKSTRING);
+		Mockito.verify(mockService).createActivity(MOCKSTRING);
+		Assert.assertEquals(MOCKSTRING, activityString);		
 	}
 	
 	@Test
 	public void testupdateActivity() {
-		fail("Not yet implemented");
+		Mockito.when(mockService.updateActivity((long) 1, MOCKSTRING)).thenReturn(MOCKSTRING);
+		String activityString = subject.updateActivityAsJSON((long) 1, MOCKSTRING);
+		Mockito.verify(mockService).updateActivity((long) 1, MOCKSTRING);
+		Assert.assertEquals(MOCKSTRING, activityString);	
 	}
 	
 	@Test
 	public void testdeleteActivity() {
-		fail("Not yet implemented");
+		Mockito.when(mockService.deleteActivity(Mockito.anyLong())).thenReturn(MOCK_DELETE_MESSAGE);
+		String activityString = subject.deleteActivityAsJSON((long) 1);
+		Mockito.verify(mockService).deleteActivity((long) 1);
+		Assert.assertEquals(MOCK_DELETE_MESSAGE, activityString);	
 	}
 
 }
